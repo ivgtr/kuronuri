8099cdeabeef945e113db4a3b525ede178846b0f
ivgtr <ivgtr.me@gmail.com>
2021-07-07 12:22:45 +0900

    :sparkles: add: 名詞を置き換える機能追加

---
+++ src/cli.ts
@@ -0,0 +1,49 @@
+import meow from "meow";
+import type { Package } from "update-notifier";
+import updateNotifier from "update-notifier";
+import { nonoun, Options } from "./index.js";
+
+export default (() => {
+  const cli = meow(
+    `
+Usage
+  $ nonoun <text> [query]
+Examples
+  $ nonoun 吾輩は猫である
+  ■■は■である
+  $ nonoun 吾輩は猫である -p ▲
+  ▲▲は▲である
+  $ nonoun 吾輩は猫である -p ▲,■,●
+  ●■は▲である
+`,
+    {
+      importMeta: import.meta,
+      flags: {
+        pack: {
+          type: "string",
+          alias: "p",
+        },
+      },
+    }
+  );
+
+  updateNotifier({ pkg: cli.pkg as Package }).notify();
+
+  const { input, flags } = cli;
+
+  const options: Options = {
+    text: input[0],
+  };
+
+  if (flags?.pack) {
+    options.pack = flags.pack.split(",");
+  }
+
+  nonoun(options)
+    .then((text) => {
+      console.log(text);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+})();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("hello");
+import { tokenize } from "kuromojin";
+
+export type Options = {
+  text: string;
+  pack?: string[];
+};
+
+export const nonoun = async (options: Options): Promise<string> => {
+  const { text, pack = ["■"] } = options;
+
+  return tokenize(text).then((tokens) => {
+    const tokenArr = tokens.map((token) => {
+      if (token.pos === "名詞")
+        return [...Array(token.surface_form.length)]
+          .map(() => pack[Math.floor(Math.random() * pack.length)])
+          .join("");
+      return token.surface_form;
+    });
+
+    return tokenArr.join("");
+  });
+};
